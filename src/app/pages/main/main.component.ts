ae1d563018934508e8f98e978b9051a1e2c626b6
Kavindu Dilshan <kavindu4543@gmail.com>
2025-04-25 11:45:52 +0530

    Message sending with emojis completed

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatListComponent } from "../../components/chat-list/chat-list.component";
-import { ChatResponse, MessageResponse } from '../../services/models';
+import { ChatResponse, MessageRequest, MessageResponse } from '../../services/models';
 import { ChatService, MessageService } from '../../services/services';
 import { KeycloakService } from '../../utils/keycloak/keycloak.service';
 import { DatePipe } from '@angular/common';
+import { PickerComponent } from "@ctrl/ngx-emoji-mart"
+import {FormsModule} from '@angular/forms';
+import { EmojiData } from '@ctrl/ngx-emoji-mart/ngx-emoji';
 
 @Component({
   selector: 'app-main',
-  imports: [ChatListComponent,DatePipe],
+  imports: [ChatListComponent,DatePipe,PickerComponent,FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent implements OnInit {
+
+
+
+
+
   chats: Array<ChatResponse> = [];
   selectedChat: ChatResponse = {};
   chatMessages: MessageResponse[] = [];
+  showEmojis:boolean = false;
+  messageContent = '';
 
   constructor(
     private readonly chatService: ChatService,
@@ -52,7 +62,11 @@ export class MainComponent implements OnInit {
   }
 
   setMessagesToSeen() {
-   
+    this.messageService.setMessagesToSeen({
+      'chat-id': this.selectedChat.id as string
+    }).subscribe({
+      next:() => {}
+    });
   }
 
   private getAllChatMessages(chatId: string) {
@@ -68,5 +82,74 @@ export class MainComponent implements OnInit {
     isSelfMessage(message: MessageResponse):boolean {
       return message.senderId === this.keycloakService.userId;
     }
+
+    uploadMediaFile(target: EventTarget|null) {
+      
+    }
+
+    onSelectEmojis(emojiSelected: any) {
+      const emoji: EmojiData = emojiSelected.emoji;
+      this.messageContent += emoji.native;
+    }
+
+    keyDown(event: KeyboardEvent) {
+      if (event.key == 'Enter') {
+          this.sendMessage();
+      }
+    }
+
+    onClick() {
+      this.setMessagesToSeen();
+    }
+
+    sendMessage() {
+      if (this.messageContent) {
+        const messageRequest: MessageRequest = {
+          chatId: this.selectedChat.id,
+          senderId: this.getSenderId(),
+          receiverId: this.getReceiverId(),
+          content: this.messageContent,
+          type: 'TEXT'
+        };
+
+        this.messageService.saveMessage({
+          body: messageRequest
+        }).subscribe({
+          next: () => {
+            const message: MessageResponse = {
+              senderId: this.getSenderId(),
+              receiverId: this.getReceiverId(),
+              content: this.messageContent,
+              type: 'TEXT',
+              state: 'SENT',
+              createdAt: new Date().toString()
+            };
+            this.selectedChat.lastMessage = this.messageContent;
+            this.chatMessages.push(message);
+            this.messageContent = '';
+            this.showEmojis = false;
+          }
+        });
+
+      }
+    }
+
+  private getSenderId(): string{
+    if (this.selectedChat.senderId === this.keycloakService.userId) {
+      return this.selectedChat.senderId as string;
+    }
+
+    return this.selectedChat.receiverId as string;
+  }
+
+  private getReceiverId(): string{
+    if (this.selectedChat.senderId === this.keycloakService.userId) {
+      return this.selectedChat.receiverId as string;
+    }
+
+    return this.selectedChat.senderId as string;
+  }
+
+
 
 }
